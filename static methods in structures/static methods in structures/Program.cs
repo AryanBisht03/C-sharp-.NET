59e9f9afcc82494402f50c60a24b620e5efd73d0
Aryan Bisht <92202194+AryanBisht03@users.noreply.github.com>
2024-10-23 20:07:36 +0530

    Add files via upload

---
+++ static methods in structures/static methods in structures/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_methods_in_structures
+{
+    struct new_class
+    {
+        public static int num(string s)
+        {
+            int number = int.Parse(s);
+            return number;
+        }
+        public static int add(int x,int y)
+        {
+            return x + y;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //new_class sr=new new_class();
+            int a = new_class.num("12");
+            int b = new_class.num("23");
+            Console.WriteLine($"The sum of {a} and {b}  is {new_class.add(a,b)}");
+
+        }
+    }
+}
