054e6e7edefb78167bda3c08c18c22aaaa9fdbfd
Aryan Bisht <92202194+AryanBisht03@users.noreply.github.com>
2024-10-24 02:40:52 +0530

    Add files via upload

---
+++ enum_from_beg2/enum_from_beg2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enum_from_beg2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
